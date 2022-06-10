0ac83ff466999279b97e6014d23d29b67dc85bc0
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-06-10 16:44:51 +0700

    MusicStoreMp3.sql (patch 0.1)

---
+++ MusicStoreMp3.sql
@@ -0,0 +1,38 @@
+Create database MusicStore
+use MusicStore
+
+Create table genre
+(
+id int primary key identity(1,1),
+name varchar(50),
+)
+
+Create table artist
+(
+id int primary key identity(1,1),
+name varchar(50),
+description varchar(50),
+image varchar(200)
+)
+
+Create table album
+(
+id int primary key identity(1,1),
+name varchar(50),
+artist_id int foreign key references artist(id),
+release_date date,
+image varchar(200)
+)
+
+Create table song
+(
+id int primary key identity(1,1),
+name varchar(50),
+media varchar(500),
+album_id int foreign key references album(id),
+artist_id int foreign key references artist(id),
+lyric ntext,
+genre_id int foreign key references genre(id),
+price int,
+[view] int
+)
