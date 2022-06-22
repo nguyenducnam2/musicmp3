7d7e9033186e6c7cc828b51fa2b83f113b4a0de4
sluuthanh <75598829+sluuthanh@users.noreply.github.com>
2022-06-22 22:19:31 +0700

    zzz

--- musicstore/src/main/java/vn/aptech/musicstore/config/WebSecurityConfig.java
+++ musicstore/src/main/java/vn/aptech/musicstore/config/WebSecurityConfig.java
@@ -7,7 +7,6 @@ package vn.aptech.musicstore.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import static org.springframework.security.authorization.AuthorityAuthorizationManager.hasRole;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,37 +21,38 @@ import vn.aptech.musicstore.service.impl.AccountServiceImpl;
  *
  * @author Thanh Sang
  */
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-     @Autowired
+
+    @Autowired
     private AccountServiceImpl accountService;
+
     @Bean
-    public PasswordEncoder encodePassword(){
+    public PasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
-    
-     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(accountService).passwordEncoder(encodePassword());
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        
+    protected void configure(final HttpSecurity http) throws Exception {
+
 //            http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/admin/login").permitAll().defaultSuccessUrl("/admin").loginProcessingUrl("/j_spring_security_check");
-            
 //        super.configure(http); //To change body of generated methods, choose Tools | Templates.
         http.csrf().disable();
 
         http.authorizeHttpRequests()
-                .antMatchers("/admin/login","logout","/client/**").permitAll();
+                .antMatchers("/admin/login", "logout", "/client/**","/**/*.js", "/**/*.css", "/**/*.mp3", "/**/*.mp4", "/**/*.jpeg", "/**/*.html").permitAll();
         http.authorizeHttpRequests()
-                .antMatchers("/admin/account/**").hasRole("ADMIN")
-                .antMatchers("/admin/**").hasAnyRole("ADMIN","USER")
+                //                .antMatchers("/").
+                .antMatchers("/admin/account/*").hasRole("ADMIN")
+                .antMatchers("/admin/*").hasAnyRole("ADMIN", "USER")
                 .anyRequest()
                 .authenticated();
-        
+
         // muon vao account/create or save thi phai la role admin
 //        http.authorizeHttpRequests().antMatchers("/admin/account/create","/admin/account/save").hasRole("ADMIN");
         //try cap route: /user =>y/c login
@@ -64,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/admin/login")
                 .usernameParameter("username")
                 .passwordParameter("password")
-                .defaultSuccessUrl("/admin",true)
+                .defaultSuccessUrl("/admin", true)
                 .failureUrl("/admin/login?error=true")
                 //cau hinh Logout Page
                 .and().logout().logoutUrl("/admin/logout")
@@ -72,14 +72,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling()
                 .accessDeniedPage("/error/403");
-       http.authorizeHttpRequests().and().rememberMe();
+        http.authorizeHttpRequests().and().rememberMe();
         //config remember
 //                .tokenRepository("abc")
 //                .tokenValiditySeconds(24*60*60); //1 ngay
     }
-    @Override
-  public void configure(WebSecurity web) {
-    web.ignoring()
-        .antMatchers("/resources/**", "/static/**");
-  }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web
+//                .ignoring()
+//                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/icon/**");
+//    }
 }
