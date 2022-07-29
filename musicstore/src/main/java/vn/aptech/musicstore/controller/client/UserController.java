21b8fa81d1ced12e5a986f083796d99d7b449610
sluuthanh <75598829+sluuthanh@users.noreply.github.com>
2022-07-29 16:18:14 +0700

    register user ok

--- musicstore/src/main/java/vn/aptech/musicstore/controller/client/UserController.java
+++ musicstore/src/main/java/vn/aptech/musicstore/controller/client/UserController.java
@@ -16,20 +16,23 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import vn.aptech.musicstore.entity.Account;
-import vn.aptech.musicstore.entity.Album;
 import vn.aptech.musicstore.entity.Song;
+import vn.aptech.musicstore.entity.model.AccountModel;
+import vn.aptech.musicstore.entity.model.UserModel;
 import vn.aptech.musicstore.repository.PasswordResetTokenRepository;
 import vn.aptech.musicstore.service.AccountService;
 import vn.aptech.musicstore.service.AlbumService;
@@ -44,22 +47,22 @@ import vn.aptech.musicstore.service.SongService;
 @RequestMapping("/user")
 @Slf4j
 public class UserController {
-
+    
     @Autowired
     private AccountService userService;
-
+    
     @Autowired
     private PasswordResetTokenRepository passwordResetTokenRepository;
-
+    
     @Autowired
     private SongService service_song;
-
+    
     @Autowired
     private AlbumService service_album;
-
+    
     @Autowired
     private ArtistService service_artist;
-
+    
     @Value("${static.base.url}")
     private String base_url;
 
@@ -83,32 +86,42 @@ public class UserController {
         return "client/user/index";
     }
 
-    @GetMapping("/profile")
-    public String profile() {
+//    @GetMapping("/profile")
+//    public String profile() {
+//        return "client/user/profile";
+//    }
+    @GetMapping("/profile/{id}")
+    public String profile(@PathVariable("id") Long id, Model model) {
+         Optional<Account> a = userService.findById(id);
+         UserModel dto = new UserModel();
+        
+        if(a.isPresent()){
+            Account entity = a.get();
+            BeanUtils.copyProperties(entity, dto);
+            dto.setEnabled(true);
+            dto.setPassword("");
+            model.addAttribute("account", dto);
+        }
+        model.addAttribute("message","Account is not exist");
         return "client/user/profile";
     }
-
+    
     @PostMapping("/processUpdate")
-//    public String processUpdate(@ModelAttribute("session.user") Account acc, @RequestParam("file") MultipartFile file) throws IOException {
-    public String processUpdate(@ModelAttribute("session.user") Account acc) throws IOException {
-        Account user = userService.findById(acc.getId()).get();
-        if (user!=null){
-            user.setLastName(acc.getLastName());
-            user.setFirstName(acc.getFirstName());
+    public String processUpdate(@ModelAttribute("account") Account user, @RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        
+        if (!(file.isEmpty())) {
+            user.setImage(file.getOriginalFilename());
+            Files.copy(file.getInputStream(), Paths.get(base_url + "\\webdata\\user" + File.separator + file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
             userService.save(user);
-        }else{
-            return "redirect:/user/profile";
+        } else {
+            user.setImage(userService.findById(user.getId()).orElseThrow().getImage());
+            userService.save(user);
         }
-            //        if (!(file.isEmpty())) {
-        ////            acc.setImage(file.getOriginalFilename());
-        ////            Files.copy(file.getInputStream(), Paths.get(base_url + "\\webdata\\acc" + File.separator + file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
-        //            userService.save(acc);
-        //        } else {
-        ////            acc.setImage(userService.findById(acc.getId()).orElseThrow().getImage());
-        //            userService.save(acc);
-        //        }
-        {
-            return "redirect:/user/profile";
-        }
+        
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
+        
+        return "redirect:/user";
+        
     }
 }
