ddc4eed177998034f3224928de4e983cc0404e6d
Dung Nguyen <DucDungvt1996@gmail.com>
2022-07-30 14:50:57 +0700

    News
    Add client

---
+++ musicstore/src/main/java/vn/aptech/musicstore/controller/client/NewsClientController.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.aptech.musicstore.controller.client;
+
+import java.util.Optional;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import vn.aptech.musicstore.entity.Account;
+import vn.aptech.musicstore.entity.News;
+import vn.aptech.musicstore.entity.model.PasswordModel;
+import vn.aptech.musicstore.entity.model.UserModel;
+import vn.aptech.musicstore.service.NewsService;
+
+/**
+ *
+ * @author Dung
+ */
+@Controller
+@RequestMapping("/news")
+public class NewsClientController {
+    @Value("${static.base.url}")
+    private String base_url;
+
+    @Autowired
+    private NewsService service;
+
+    @GetMapping
+    public String index(Model model, @RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber,
+            @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
+        model.addAttribute("list", service.getPage(pageNumber, size));
+        model.addAttribute("service", service);
+        model.addAttribute("title", "null");
+        return "client/news/index";
+    }
+    
+    @GetMapping("/search")
+    public String search(Model model, @RequestParam("title") String title, @RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber,
+            @RequestParam(value = "size", required = false, defaultValue = "1000") int size) {
+        model.addAttribute("list", service.getPage(pageNumber, size));
+        model.addAttribute("service", service);
+        model.addAttribute("title", title);
+        return "client/news/index";
+    }
+    
+    @GetMapping("/details/{id}")
+    public String details(Model model, @PathVariable("id") int id) {
+        model.addAttribute("list", service.findById(id).orElseThrow());
+        return "client/news/details";
+    }
+}
