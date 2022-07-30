195fd42e0983740a8962666f7e1a48aaed5cf7b0
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-07-30 21:38:31 +0700

    client add song to playlist ,not done vi met

---
+++ musicstore/src/main/java/vn/aptech/musicstore/repository/PlaylistRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.aptech.musicstore.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import vn.aptech.musicstore.entity.Playlist;
+
+/**
+ *
+ * @author namng
+ */
+public interface PlaylistRepository extends JpaRepository<Playlist,Integer>{
+    @Query("SELECT o FROM Playlist o WHERE o.accountId=:accountId")
+    List<Playlist> findByAccountId(@Param("accountId") Long accountId);
+}
---
+++ musicstore/src/main/java/vn/aptech/musicstore/service/impl/PlaylistitemServiceImpl.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.aptech.musicstore.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vn.aptech.musicstore.entity.Playlistitem;
+import vn.aptech.musicstore.repository.PlaylistitemRepository;
+import vn.aptech.musicstore.service.PlaylistitemService;
+
+/**
+ *
+ * @author namng
+ */
+@Service
+public class PlaylistitemServiceImpl implements PlaylistitemService {
+
+    @Autowired
+    private PlaylistitemRepository repo;
+
+    @Override
+    public List<Playlistitem> findByPlaylistId(int playlistId) {
+        return repo.findByPlaylistId(playlistId);
+    }
+
+    @Override
+    public Playlistitem save(Playlistitem plt) {
+        return repo.save(plt);
+    }
+
+}
