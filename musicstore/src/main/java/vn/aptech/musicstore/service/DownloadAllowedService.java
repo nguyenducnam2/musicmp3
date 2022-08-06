9adb61eeeb2e2ce5b46ae17cdf27d3d32a315101
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-08-06 15:57:10 +0700

    UPDATE FILE .SQL

---
+++ musicstore/src/main/java/vn/aptech/musicstore/service/DownloadAllowedService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.aptech.musicstore.service;
+
+import java.util.List;
+import vn.aptech.musicstore.entity.DownloadAllowed;
+
+/**
+ *
+ * @author namng
+ */
+public interface DownloadAllowedService {
+    List<DownloadAllowed> findAll();
+}
