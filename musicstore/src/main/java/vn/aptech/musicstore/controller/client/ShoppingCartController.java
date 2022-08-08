014a58cad9d7a7b7d86db6caa4eb3891fb183321
HuanJiFeng <terminatau@gmail.com>
2022-08-08 14:50:40 +0700

    fix tam thoi

--- musicstore/src/main/java/vn/aptech/musicstore/controller/client/ShoppingCartController.java
+++ musicstore/src/main/java/vn/aptech/musicstore/controller/client/ShoppingCartController.java
@@ -1,79 +1,79 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package vn.aptech.musicstore.controller.client;
-
-import static java.nio.file.Files.list;
-import java.util.Collection;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import vn.aptech.musicstore.entity.Product;
-import vn.aptech.musicstore.service.ProductService;
-import vn.aptech.musicstore.service.ShoppingCartService;
-
-/**
- *
- * @author Dung
- */
-@Controller
-@RequestMapping("cart")
-public class ShoppingCartController {
-
-    @Autowired
-    ProductService service;
-
-    @Autowired
-    ShoppingCartService cartService;
-
-    @GetMapping("index")
-    public String index(Model model) {
-        Collection<Product> products = cartService.getProducts();
-        model.addAttribute("products", products);
-        model.addAttribute("amount", cartService.getAmount());
-        model.addAttribute("count", cartService.getCount());
-        return "client/cart/index";
-    }
-
-    @GetMapping("add/{id}")
-    public String add(@PathVariable("id") Integer id) {
-        Optional<Product> product = service.findById(id);
-        Product products = product.get();
-        if (products != null) {
-//            products.setQuantity(1);
-            cartService.add(products);
-        }
-        return "redirect:/cart/index";
-    }
-
-    @GetMapping("remove/{id}")
-    public String remove(@PathVariable("id") Integer id) {
-        cartService.remove(id);
-        return "redirect:/cart/index";
-    }
-
-    @PostMapping("update")
-    public String update(@RequestParam("id") Integer id, @RequestParam("quantity") int quantity) {
-        cartService.update(id, quantity);
-        return "redirect:/cart/index";
-    }
-
-    @GetMapping("clear")
-    public String clear() {
-        cartService.clear();
-        return "redirect:/cart/index";
-    }
-
-//    @Autowired
-//    ProductService productService;
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package vn.aptech.musicstore.controller.client;
 //
+//import static java.nio.file.Files.list;
+//import java.util.Collection;
+//import java.util.Optional;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import vn.aptech.musicstore.entity.Product;
+//import vn.aptech.musicstore.service.ProductService;
+//import vn.aptech.musicstore.service.ShoppingCartService;
+//
+///**
+// *
+// * @author Dung
+// */
+//@Controller
+//@RequestMapping("cart")
+//public class ShoppingCartController {
+//
+//    @Autowired
+//    ProductService service;
+//
+//    @Autowired
+//    ShoppingCartService cartService;
+//
+//    @GetMapping("index")
+//    public String index(Model model) {
+//        Collection<Product> products = cartService.getProducts();
+//        model.addAttribute("products", products);
+//        model.addAttribute("amount", cartService.getAmount());
+//        model.addAttribute("count", cartService.getCount());
+//        return "client/cart/index";
+//    }
+//
+//    @GetMapping("add/{id}")
+//    public String add(@PathVariable("id") Integer id) {
+//        Optional<Product> product = service.findById(id);
+//        Product products = product.get();
+//        if (products != null) {
+////            products.setQuantity(1);
+//            cartService.add(products);
+//        }
+//        return "redirect:/cart/index";
+//    }
+//
+//    @GetMapping("remove/{id}")
+//    public String remove(@PathVariable("id") Integer id) {
+//        cartService.remove(id);
+//        return "redirect:/cart/index";
+//    }
+//
+//    @PostMapping("update")
+//    public String update(@RequestParam("id") Integer id, @RequestParam("quantity") int quantity) {
+//        cartService.update(id, quantity);
+//        return "redirect:/cart/index";
+//    }
+//
+//    @GetMapping("clear")
+//    public String clear() {
+//        cartService.clear();
+//        return "redirect:/cart/index";
+//    }
+//
+////    @Autowired
+////    ProductService productService;
+////
 //    @Autowired
 //    CategoryService categoryService;
 //
