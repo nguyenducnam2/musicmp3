9df5630baaf5abbb1cbbc646cdcc8acc9951d23e
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-08-12 22:50:05 +0700

    song order detail

--- musicstore/src/main/java/vn/aptech/musicstore/controller/admin/SongOrderController.java
+++ musicstore/src/main/java/vn/aptech/musicstore/controller/admin/SongOrderController.java
@@ -5,12 +5,17 @@
  */
 package vn.aptech.musicstore.controller.admin;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import vn.aptech.musicstore.entity.SongOrderDetail;
+import vn.aptech.musicstore.service.SongOrderDetailService;
 import vn.aptech.musicstore.service.SongOrderService;
 
 /**
@@ -23,11 +28,27 @@ public class SongOrderController {
 
     @Autowired
     private SongOrderService service;
+    
+    @Autowired
+    private SongOrderDetailService serviceDt;
 
     @GetMapping
     public String findAll(Model model, @RequestParam(value = "pageNumber", required = false, defaultValue = "1") int pageNumber,
             @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
         model.addAttribute("list", service.getPage(pageNumber, size));
         return "admin/songorder/index";
+    }
+    
+    @GetMapping("/{id}")
+    public String findById(Model model,@PathVariable("id")int id){
+        List<SongOrderDetail> list=new ArrayList<>();
+        for(SongOrderDetail item:serviceDt.findAll()){
+            if(item.getSongOrderId()==id){
+                list.add(item);
+            }
+        }
+        model.addAttribute("list",list);
+        model.addAttribute("songorder", service.findById(id).get());
+        return "admin/songorder/detail";
     }
 }
