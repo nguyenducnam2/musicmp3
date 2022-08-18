0e832610537f4d8bc4a03afbe202aff99b9b761e
sluuthanh <75598829+sluuthanh@users.noreply.github.com>
2022-08-18 15:21:06 +0700

    zzz

---
+++ musicstore/src/main/java/vn/aptech/musicstore/controller/admin/PromotionController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.aptech.musicstore.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Administrator
+ */
+@Controller
+@RequestMapping("/admin/promotion")
+public class PromotionController {
+    
+}
