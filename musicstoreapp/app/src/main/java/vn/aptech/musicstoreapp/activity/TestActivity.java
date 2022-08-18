2ed8e4b5afcee16c0189264abc3f764c33842320
nguyenducnam2 <98157595+nguyenducnam2@users.noreply.github.com>
2022-08-18 15:12:46 +0700

    test

---
+++ musicstoreapp/app/src/main/java/vn/aptech/musicstoreapp/activity/TestActivity.java
@@ -0,0 +1,16 @@
+package vn.aptech.musicstoreapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vn.aptech.musicstoreapp.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
